92898b1278dc2be762796ad439dffbb430ac666d
amog787 <amoghmaiya@gmail.com>
2019-08-28 18:52:18 +0530

    init

---
+++ FingerprintUnlockController.java
@@ -0,0 +1,223 @@
+package com.android.systemui.statusbar.phone;
+
+import android.app.ActivityManagerNative;
+import android.content.Context;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.SystemClock;
+import android.os.Trace;
+import android.provider.MiuiSettings.System;
+import android.util.Log;
+import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.keyguard.KeyguardUpdateMonitorCallback;
+import com.android.keyguard.LatencyTracker;
+import com.android.systemui.Dependency;
+import com.android.systemui.keyguard.KeyguardViewMediator;
+
+public class FingerprintUnlockController extends KeyguardUpdateMonitorCallback {
+    private final Context mContext;
+    private DozeScrimController mDozeScrimController;
+    private Handler mHandler = new Handler();
+    private KeyguardViewMediator mKeyguardViewMediator;
+    private int mMode;
+    private int mPendingAuthenticatedUserId = -1;
+    private PowerManager mPowerManager;
+    private final Runnable mReleaseFingerprintWakeLockRunnable = new 1(this);
+    private ScrimController mScrimController;
+    private StatusBar mStatusBar;
+    private StatusBarKeyguardViewManager mStatusBarKeyguardViewManager;
+    private StatusBarWindowManager mStatusBarWindowManager;
+    private final UnlockMethodCache mUnlockMethodCache;
+    private KeyguardUpdateMonitor mUpdateMonitor;
+    private WakeLock mWakeLock;
+
+    public FingerprintUnlockController(Context context, DozeScrimController dozeScrimController, KeyguardViewMediator keyguardViewMediator, ScrimController scrimController, StatusBar statusBar, UnlockMethodCache unlockMethodCache) {
+        this.mContext = context;
+        this.mPowerManager = (PowerManager) context.getSystemService(PowerManager.class);
+        this.mUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
+        this.mUpdateMonitor.registerCallback(this);
+        this.mStatusBarWindowManager = (StatusBarWindowManager) Dependency.get(StatusBarWindowManager.class);
+        this.mDozeScrimController = dozeScrimController;
+        this.mKeyguardViewMediator = keyguardViewMediator;
+        this.mScrimController = scrimController;
+        this.mStatusBar = statusBar;
+        this.mUnlockMethodCache = unlockMethodCache;
+    }
+
+    private int calculateMode(int i) {
+        boolean isUnlockingWithFingerprintAllowed = this.mUpdateMonitor.isUnlockingWithFingerprintAllowed(i);
+        if (!this.mUpdateMonitor.isDeviceInteractive()) {
+            return !this.mStatusBarKeyguardViewManager.isShowing() ? 4 : (this.mDozeScrimController.isPulsing() && isUnlockingWithFingerprintAllowed) ? 2 : (isUnlockingWithFingerprintAllowed || !this.mUnlockMethodCache.isMethodSecure()) ? 1 : 3;
+        } else {
+            if (this.mStatusBarKeyguardViewManager.isShowing()) {
+                if (this.mStatusBarKeyguardViewManager.isBouncerShowing() && isUnlockingWithFingerprintAllowed) {
+                    return 6;
+                }
+                if (isUnlockingWithFingerprintAllowed) {
+                    return 5;
+                }
+                if (!this.mStatusBarKeyguardViewManager.isBouncerShowing()) {
+                    return 3;
+                }
+            }
+            return 0;
+        }
+    }
+
+    private void cleanup() {
+        releaseFingerprintWakeLock();
+    }
+
+    private void keyguardDoneWithoutHomeAnim() {
+        System.putBooleanForUser(this.mContext.getContentResolver(), "is_fingerprint_unlock", true, -2);
+    }
+
+    private void releaseFingerprintWakeLock() {
+        if (this.mWakeLock != null) {
+            this.mHandler.removeCallbacks(this.mReleaseFingerprintWakeLockRunnable);
+            Log.i("FingerprintController", "releasing fp wakelock");
+            this.mWakeLock.release();
+            this.mWakeLock = null;
+        }
+    }
+
+    public void finishKeyguardFadingAway() {
+        this.mMode = 0;
+        this.mUpdateMonitor.setFingerprintMode(this.mMode);
+        this.mStatusBarWindowManager.setForceDozeBrightness(false);
+        if (this.mStatusBar.getNavigationBarView() != null) {
+            this.mStatusBar.getNavigationBarView().setWakeAndUnlocking(false);
+        }
+        this.mStatusBar.notifyFpAuthModeChanged();
+    }
+
+    public int getMode() {
+        return this.mMode;
+    }
+
+    public void onFingerprintAcquired() {
+        Trace.beginSection("FingerprintUnlockController#onFingerprintAcquired");
+        releaseFingerprintWakeLock();
+        if (!this.mUpdateMonitor.isDeviceInteractive()) {
+            if (LatencyTracker.isEnabled(this.mContext)) {
+                LatencyTracker.getInstance(this.mContext).onActionStart(2);
+            }
+            this.mWakeLock = this.mPowerManager.newWakeLock(1, "wake-and-unlock wakelock");
+            Trace.beginSection("acquiring wake-and-unlock");
+            this.mWakeLock.acquire();
+            Trace.endSection();
+            Log.i("FingerprintController", "fingerprint acquired, grabbing fp wakelock");
+            this.mHandler.postDelayed(this.mReleaseFingerprintWakeLockRunnable, 15000);
+            if (this.mDozeScrimController.isPulsing()) {
+                this.mStatusBarWindowManager.setForceDozeBrightness(true);
+            }
+        }
+        Trace.endSection();
+    }
+
+    public void onFingerprintAuthFailed() {
+        cleanup();
+    }
+
+    public void onFingerprintAuthenticated(int i) {
+        Trace.beginSection("FingerprintUnlockController#onFingerprintAuthenticated");
+        if (this.mUpdateMonitor.isGoingToSleep()) {
+            this.mPendingAuthenticatedUserId = i;
+            Trace.endSection();
+            return;
+        }
+        boolean isDeviceInteractive = this.mUpdateMonitor.isDeviceInteractive();
+        this.mMode = calculateMode(i);
+        this.mUpdateMonitor.setFingerprintMode(this.mMode);
+        if (!(KeyguardUpdateMonitor.getCurrentUser() == i || this.mMode == 3 || this.mMode == 0 || this.mMode == 4)) {
+            try {
+                ActivityManagerNative.getDefault().switchUser(i);
+            } catch (Throwable e) {
+                Log.e("FingerprintController", "switchUser failed", e);
+            }
+        }
+        PanelBar.LOG(getClass(), "calculateMode userid=0;mode=" + this.mMode);
+        if (!isDeviceInteractive) {
+            Log.i("FingerprintController", "fp wakelock: Authenticated, waking up...");
+            this.mPowerManager.wakeUp(SystemClock.uptimeMillis(), "android.policy:FINGERPRINT");
+        }
+        Trace.beginSection("release wake-and-unlock");
+        releaseFingerprintWakeLock();
+        Trace.endSection();
+        switch (this.mMode) {
+            case 1:
+            case 2:
+                if (this.mMode == 2) {
+                    Trace.beginSection("MODE_WAKE_AND_UNLOCK_PULSING");
+                    this.mStatusBar.updateMediaMetaData(false, true);
+                } else {
+                    Trace.beginSection("MODE_WAKE_AND_UNLOCK");
+                    this.mDozeScrimController.abortDoze();
+                }
+                keyguardDoneWithoutHomeAnim();
+                this.mStatusBarWindowManager.setStatusBarFocusable(false);
+                this.mKeyguardViewMediator.onWakeAndUnlocking();
+                this.mScrimController.setWakeAndUnlocking();
+                this.mDozeScrimController.setWakeAndUnlocking();
+                if (this.mStatusBar.getNavigationBarView() != null) {
+                    this.mStatusBar.getNavigationBarView().setWakeAndUnlocking(true);
+                }
+                Trace.endSection();
+                break;
+            case 3:
+                Trace.beginSection("MODE_SHOW_BOUNCER");
+                if (!isDeviceInteractive) {
+                    this.mStatusBarKeyguardViewManager.notifyDeviceWakeUpRequested();
+                }
+                this.mStatusBarKeyguardViewManager.animateCollapsePanels(0.0f);
+                Trace.endSection();
+                break;
+            case 5:
+                Trace.beginSection("MODE_UNLOCK");
+                keyguardDoneWithoutHomeAnim();
+                if (!isDeviceInteractive) {
+                    this.mStatusBarKeyguardViewManager.notifyDeviceWakeUpRequested();
+                }
+                this.mStatusBarWindowManager.setStatusBarFocusable(false);
+                this.mKeyguardViewMediator.keyguardDone();
+                Trace.endSection();
+                break;
+            case 6:
+                Trace.beginSection("MODE_DISMISS");
+                this.mStatusBarKeyguardViewManager.notifyKeyguardAuthenticated(false);
+                Trace.endSection();
+                break;
+        }
+        if (this.mMode != 2) {
+            this.mStatusBarWindowManager.setForceDozeBrightness(false);
+        }
+        this.mStatusBar.notifyFpAuthModeChanged();
+        Trace.endSection();
+    }
+
+    public void onFingerprintError(int i, String str) {
+        cleanup();
+    }
+
+    public void onFinishedGoingToSleep(int i) {
+        Trace.beginSection("FingerprintUnlockController#onFinishedGoingToSleep");
+        if (this.mPendingAuthenticatedUserId != -1) {
+            this.mHandler.post(new 2(this, this.mPendingAuthenticatedUserId));
+        }
+        this.mPendingAuthenticatedUserId = -1;
+        Trace.endSection();
+    }
+
+    public void onStartedGoingToSleep(int i) {
+        this.mPendingAuthenticatedUserId = -1;
+    }
+
+    public void setStatusBarKeyguardViewManager(StatusBarKeyguardViewManager statusBarKeyguardViewManager) {
+        this.mStatusBarKeyguardViewManager = statusBarKeyguardViewManager;
+    }
+
+    public void startKeyguardFadingAway() {
+        this.mHandler.postDelayed(new 3(this), 96);
+    }
+}
